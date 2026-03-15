a552c0c3b28e9ae08dbd54cd4d028facfca215d1
Susanna Mandalapudi <mandalapudisusanna@gmail.com>
2026-03-15 22:21:55 +0530

    Add files via upload

---
+++ after-spring-aop-demo-one/src/main/java/com/javatpoint/TrackOperation.java
@@ -0,0 +1,19 @@
+package com.javatpoint;  
+  
+import org.aspectj.lang.JoinPoint;  
+import org.aspectj.lang.annotation.Aspect;  
+import org.aspectj.lang.annotation.After;  
+import org.aspectj.lang.annotation.Pointcut;  
+  
+@Aspect  
+public class TrackOperation{  
+    @Pointcut("execution(* Operation.*(..))")  
+    public void k(){}//pointcut name  
+      
+    @After("k()")//applying pointcut on after advice  
+    public void myadvice(JoinPoint jp)//it is advice (after advice)  
+    {  
+        System.out.println("additional concern");  
+        //System.out.println("Method Signature: "  + jp.getSignature());  
+    }  
+}  
